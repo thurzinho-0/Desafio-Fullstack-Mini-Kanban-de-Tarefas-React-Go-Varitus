dc1ecb5bd890a733fa1f9842302deeeaef02ce6a
Arthur Marques <arthurdearaujomarques@gmail.com>
2025-11-12 21:26:42 -0300

    Create models.go

---
+++ backend/models.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type Task struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description,omitempty"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
